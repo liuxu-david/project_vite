0bc95d6b71d5d50aa147f82179c4ab68516f4bb2
David <2982359231@qq.com>
2024-07-10 15:20:31 +0800

    虚拟列表功能完善

--- src/views/list/index.vue
+++ src/views/list/index.vue
@@ -1,84 +1,85 @@
 <script setup>
-  import { computed, onMounted, reactive, ref } from "vue";
+import {computed, onMounted, reactive, ref} from "vue";
 
-  // 数据定义
-  const listRef = ref(null);
-  let scrollTop = ref(0);
-  let viewHeight = 0;
-  let itemHeight = 30;
-  let itemSum = ref(0);
-  let startIndex = ref(0);
+// 数据定义
+const listRef = ref(null);
+let scrollTop = ref(0);
+let viewHeight = 0;
+let itemHeight = 30;
+let itemSum = ref(0);
+let startIndex = ref(0);
 
-  // 后端返回的数据
-  const data = [];
-  for (let i = 1; i <= 200; i++) {
-    data.push({ id: i, content: `列表${i}` });
-  }
-  // 计算属性
-  const handleListResult = computed(() => {
-    const endIndex = startIndex.value + itemSum.value;
-    endIndex > data.length ? data.length : endIndex;
-    const result = data.slice(startIndex.value, endIndex);
-    console.log(result);
-    console.log(startIndex.value, endIndex);
-    return result;
-  });
-  // 数据监听
-  // 生命周期钩子
-  onMounted(() => {
-    viewHeight = listRef.value.offsetHeight;
-    itemSum.value = Math.ceil(viewHeight / itemHeight);
-  });
+// 后端返回的数据
+const data = [];
+for (let i = 1; i <= 200; i++) {
+  data.push({id: i, content: `列表${i}`});
+}
+// 计算属性
+const handleListResult = computed(() => {
+  const endIndex = startIndex.value + itemSum.value;
+  endIndex > data.length ? data.length : endIndex;
+  const result = data.slice(startIndex.value, endIndex);
+  console.log(result);
+  console.log(startIndex.value, endIndex);
+  return result;
+});
+// 数据监听
+// 生命周期钩子
+onMounted(() => {
+  viewHeight = listRef.value.offsetHeight;
+  itemSum.value = Math.ceil(viewHeight / itemHeight);
+});
 
-  // 方法处理
-  const handleListScroll = () => {
-    scrollTop.value = listRef.value.scrollTop;
-    startIndex.value = Math.floor(scrollTop.value / itemHeight);
+// 方法处理
+const handleListScroll = () => {
+  scrollTop.value = listRef.value.scrollTop;
+  startIndex.value = Math.floor(scrollTop.value / itemHeight);
+};
+const throttle = (fn, dalay) => {
+  let timer = null;
+  return function (...agrgs) {
+    if (timer) return;
+    timer = setTimeout(() => {
+      fn.apply(this, agrgs);
+      timer = null;
+    }, dalay);
   };
-  const throttle = (fn, dalay) => {
-    let timer = null;
-    return function (...agrgs) {
-      if (timer) return;
-      timer = setTimeout(() => {
-        fn.apply(this, agrgs);
-        timer = null;
-      }, dalay);
-    };
-  };
-  const handleThrottleScroll = throttle(handleListScroll, 100);
+};
+const handleThrottleScroll = throttle(handleListScroll, 100);
 </script>
 
 <template>
   <div class="list" @scroll="handleThrottleScroll" ref="listRef">
-    <ul
-      :style="{
-        height: `${itemHeight * data.length}px`,
+    <div :style="{height: `${itemHeight * data.length}px`,}">
+      <ul
+          :style="{
         transform: `translateY(${startIndex * itemHeight}px)`,
       }"
-    >
-      <li class="list_item" v-for="item in handleListResult" :key="item.id">
-        {{ item.content }}
-      </li>
-    </ul>
+      >
+        <li class="list_item" v-for="item in handleListResult" :key="item.id">
+          {{ item.content }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <style scoped lang="less">
-  .list {
-    width: 310px;
-    height: 320px;
-    border: 1px solid red;
-    overflow-y: scroll;
+.list {
+  width: 310px;
+  height: 320px;
+  border: 1px solid red;
+  overflow-y: scroll;
 
-    .list_item {
-      height: 30px;
-      width: 300px;
-      border: 1px solid green;
-      margin-left: -10px;
-      list-style: none;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
+  .list_item {
+    height: 30px;
+    width: 300px;
+    border: 1px solid green;
+    margin-left: -10px;
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
+}
 </style>
