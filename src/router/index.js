90aae438cef06e2c15c79b7f015163a3b27ee81a
liuxu <2982359231@qq.com>
2025-04-21 22:29:52 +0800

    build:test6

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+    createRouter,
+    createWebHistory,
+    createWebHashHistory,
+} from "vue-router";
 
 const routes = [
     {
@@ -57,7 +61,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
 
